70a944203586e078ef627bc55d4ff8c40084b656
Callen Wilson <callencolt@gmail.com>
2025-08-07 15:52:59 -0700

    feat: create Player class with triangle rendering; update constants with PLAYER_RADIUS and mainPlayer instantiation for integration

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+
+    player = Player(x, y)
+    
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     print("Starting Asteroids!")
-    
 
 if __name__ == "__main__":
     main()
